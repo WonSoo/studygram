0623f889374ed7b548d08a7be4a3070005b05c06
graykick <graykick@naver.com>
2017-06-19 09:54:18 +0900

    search form event
    search form event

--- studygram/src/components/Search.js
+++ studygram/src/components/Search.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import Axios from 'axios';
+
 const propTypes = {
 };
 const defaultProps = {
@@ -6,11 +8,27 @@ const defaultProps = {
 class Search extends Component {
     constructor(props) {
         super(props);
+        this.onTextInputChange = this.onTextInputChange.bind(this);
+        this.state = {
+            keyWord: ""
+        }
     }
+
+    onTextInputChange(e) {
+        this.setState({
+            keyWord: e.target.value
+        });
+    }
+
+    sendSearchKeyWord() {
+
+    }
+
     render() {
         return(
             <div className="Search-div">
-                <input className="Search-input" type="text" placeholder="검색"/>
+                <input className="Search-input" type="text" placeholder="검색" onChange={this.onTextInputChange}/>
+
             </div>
         );
     }
